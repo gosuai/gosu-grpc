6c25ed8c01c24f58fe04d5e843b9ce2bd2a9dddc
Slavik Upyachkovich <sg@gosu.ai>
2020-07-21 12:42:11 +0300

    fixup! Meet the gosu grpc package

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+shopt -s globstar
+
 rm -rf ./google/*
 
 python3 -m grpc_tools.protoc \
        --proto_path=googleapis \
        --python_out=. \
        --grpclib_python_out=. \
-       $(find googleapis/google/cloud/{dialogflow/v2,texttospeech/v1,translate/v3} -iname "*.proto")
+       googleapis/google/cloud/{dialogflow/v2,texttospeech/v1,translate/v3}/**/*.proto
