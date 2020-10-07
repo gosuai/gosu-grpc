63ff94adcc19a874ab3ed331f526f6041bda4c0a
Nikolay Bryskin <nb@gosu.ai>
2020-10-07 15:52:39 +0300

    Add Google STT API

--- bin/build.sh
+++ bin/build.sh
@@ -8,4 +8,4 @@ python3 -m grpc_tools.protoc \
        --proto_path=googleapis \
        --python_out=. \
        --grpclib_python_out=. \
-       googleapis/google/cloud/{dialogflow/v2,texttospeech/v1,translate/v3}/**/*.proto
+       googleapis/google/cloud/{dialogflow/v2,texttospeech/v1,translate/v3,speech/v1}/**/*.proto
